52460df47753819ed04f57c6ec659086d8ef9c55
calton momaya <nyankieyacalton@gmail.com>
2026-03-21 06:54:46 +0300

    initialized model packages

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.models.product import Product
+from app.models.category import Category
+from app.models.supplier import Supplier
+from app.models.transaction import Transaction
+
+__all__ = ['User', 'Product', 'Category', 'Supplier', 'Transaction']
